caed65c11e786da92af1914419d15cfac0548355
elijah amoako <elijahamoako72@gmail.com>
2022-09-01 13:34:44 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      spacing:{
+        "big":"48rem"
+      }
+    },
+  },
+  plugins: [],
+}
